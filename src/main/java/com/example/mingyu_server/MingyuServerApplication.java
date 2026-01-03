da0c2999530a0379334603276656304d36fe4cf5
mingyu210 <baemingyu5@gamil.com>
2026-01-03 14:13:33 +0900

    first commit

---
+++ src/main/java/com/example/mingyu_server/MingyuServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mingyu_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingyuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MingyuServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mingyu_server/MingyuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mingyu_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingyuServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
